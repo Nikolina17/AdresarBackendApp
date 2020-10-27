cad5ca156970df3e803511fccd5c633740ee645b
Nikolina Ajduk <najduk@pmfst.hr>
2020-10-27 20:25:29 +0100

    Backend vj7

--- models/poruka.js
+++ models/poruka.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const password = process.env.ATLAS_PASS
 const dbname = 'adresar-api'
 const url = `mongodb+srv://Nikolina:${password}@cluster0.k3qu1.mongodb.net/${dbname}?retryWrites=true&w=majority`
-//const url = 'mongodb+srv://Nikolina:${<password>}@cluster0.k3qu1.mongodb.net/${dbname}?retryWrites=true&w=majority'
 console.log("Spajamo se na bazu")
 
 mongoose.connect(url, {
